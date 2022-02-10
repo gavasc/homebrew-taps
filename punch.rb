b5f209b686818eeeefaca7e8a821af87649869c8
Gabriel Vasconcelos <gavascdev@gmail.com>
2022-02-10 15:45:05 -0300

    changing repository version

--- punch.rb
+++ punch.rb
@@ -1,7 +1,7 @@
 class Punch < Formula
   desc 'A digital, CLI punchcard app to control the time spent on your tasks'
   homepage 'https://github.com/Gabrielvsm/punch'
-  url 'https://github.com/Gabrielvsm/punch/archive/refs/tags/v1.0.1.tar.gz'
+  url 'https://github.com/Gabrielvsm/punch/archive/refs/tags/v1.0.2.tar.gz'
   sha256 'c4d0b75dffc52e19e94e09080f102f799ce5f89aa0f5d4d67c940bd457babbc5'
 
   def install
