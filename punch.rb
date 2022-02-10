411e586ca7196600dcdbb86a6161babbba69f718
Gabriel Vasconcelos <gavascdev@gmail.com>
2022-02-09 22:44:29 -0300

    formula for the "punch" app

---
+++ punch.rb
@@ -0,0 +1,12 @@
+class Punch < Formula
+  desc 'A digital, CLI punchcard app to control the time spent on your tasks'
+  homepage 'https://github.com/Gabrielvsm/punch'
+  url 'https://github.com/Gabrielvsm/punch/archive/refs/tags/v1.0.1.tar.gz'
+  sha256 'c4d0b75dffc52e19e94e09080f102f799ce5f89aa0f5d4d67c940bd457babbc5'
+
+  def install
+    share.install 'share/database.db'
+    lib.install 'lib/database.rb'
+    bin.install 'bin/punch'
+  end
+end
