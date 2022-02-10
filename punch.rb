5684923999ad45a5ce25000587c8633b5584e971
Gabriel Vasconcelos <gavascdev@gmail.com>
2022-02-10 15:49:53 -0300

    changing sha256

--- punch.rb
+++ punch.rb
@@ -2,7 +2,7 @@ class Punch < Formula
   desc 'A digital, CLI punchcard app to control the time spent on your tasks'
   homepage 'https://github.com/Gabrielvsm/punch'
   url 'https://github.com/Gabrielvsm/punch/archive/refs/tags/v1.0.2.tar.gz'
-  sha256 'c4d0b75dffc52e19e94e09080f102f799ce5f89aa0f5d4d67c940bd457babbc5'
+  sha256 'fbc1df327659f4df1026feb6d93ae60acdc3940b5f2c0a6e5862cb8b065e296b'
 
   def install
     share.install 'share/database.db'
